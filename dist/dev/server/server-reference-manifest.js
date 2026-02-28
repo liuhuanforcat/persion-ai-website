446ce9e809b12a7c7eb0aa6a45d133df08172b3f
liuhuan <1057016137@qq.com>
2026-02-28 16:25:21 +0800

    fix:update

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+SLecMy9Ngbf8P7pkRBeLKEpdDLfjQ8ul7P8z9R0GuM=\"\n}"
--- dist/dev/static/chunks/src_app_about_page_tsx_4865b585._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_38c8b5d5._.js",
-  "static/chunks/node_modules_dfb43654._.js"
-],
-    source: "dynamic"
-});
